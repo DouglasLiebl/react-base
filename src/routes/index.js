a66572a7d1720a3bbf5655d0dd85449e8781a7fc
Douglas Liebl <douglasliebl@outlook.com>
2024-03-08 21:11:39 -0300

    Fix: history problems

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/login';
 import Page404 from '../pages/404';
 
+import history from '../services/history';
+
 export default function PageRoutes() {
   return (
-    <Routes>
-      <Route exact path="/" Component={Login} />
-      <Route path="*" Component={Page404} />
+    <Routes history={history}>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
