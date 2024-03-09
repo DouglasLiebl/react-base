ab138dab216ccb6df80fe21d21fa1a0a06a38a16
Douglas Liebl <douglasliebl@outlook.com>
2024-03-09 18:51:30 -0300

    Add: custom private route

---
+++ src/routes/MyRoutes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useLocation, Navigate } from 'react-router-dom';
+
+export default function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = false; // Substitua isso pela sua lógica de autenticação
+  const location = useLocation();
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Navigate
+        to={{ pathname: '/login', state: { prevPath: location.pathname } }}
+      />
+    );
+  }
+
+  return <Component />;
+}
+
+MyRoute.defaultProps = {
+  isClosed: false,
+};
+
+MyRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  isClosed: PropTypes.bool,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import MyRoute from './MyRoutes';
 import Login from '../pages/login';
 import Page404 from '../pages/404';
 
@@ -9,8 +10,8 @@ import history from '../services/history';
 export default function PageRoutes() {
   return (
     <Routes history={history}>
-      <Route path="/" element={<Login />} />
-      <Route path="*" element={<Page404 />} />
+      <Route path="/" element={<MyRoute component={Login} />} />
+      <Route path="*" element={<MyRoute component={Page404} />} />
     </Routes>
   );
 }
